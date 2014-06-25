fd6a2c8d8f4f6381998be126d89f98559776af66
Michael Deng <michaeldeng18@gmail.com>
2014-06-25 14:44:23 -0700

    Push menu

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -20,24 +20,4 @@ function checkitem()
   } 
 }
 
-// Push menu
 
-var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
-        menuRight = document.getElementById( 'cbp-spmenu-s2' ),
-        menuTop = document.getElementById( 'cbp-spmenu-s3' ),
-        menuBottom = document.getElementById( 'cbp-spmenu-s4' ),
-        showLeftPush = document.getElementById( 'showLeftPush' ),
-        body = document.body;
- 
-showLeftPush.onclick = function() {
-    classie.toggle( this, 'active' );
-    classie.toggle( body, 'cbp-spmenu-push-toright' );
-    classie.toggle( menuLeft, 'cbp-spmenu-open' );
-    disableOther( 'showLeftPush' );
-};
-
-function disableOther( button ) {
-    if( button !== 'showLeftPush' ) {
-        classie.toggle( showLeftPush, 'disabled' );
-    }
-}
